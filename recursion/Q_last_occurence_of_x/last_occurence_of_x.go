a51675cc2c9e7bfbd75faec43d1c6704092b2e39
Raman <priyanshu00268@gmail.com>
2026-05-09 12:07:38 +0530

    last_occuurence_of_x using recursion

---
+++ recursion/Q_last_occurence_of_x/last_occurence_of_x.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
+
+func main() {
+    reader := bufio.NewReader(os.Stdin)
+    writer := bufio.NewWriter(os.Stdout)
+    defer writer.Flush()
+
+    var t int
+    fmt.Fscan(reader, &t)
+
+    slice := make([]int, t)
+    for i := 0; i < t; i++ {
+    	fmt.Fscan(reader, &slice[i])
+    }
+
+    var x int
+    fmt.Fscan(reader, &x)
+
+    ans := f(slice, t, x)
+    if ans == -1 {
+    	fmt.Fprintln(writer, ans)
+    }else {
+    	fmt.Fprintln(writer, ans+1)
+    }
+}
+
+func f(slice []int, n, x int) int{
+	if n == 0 {
+		return -1
+	}
+
+	if slice[n-1] == x {
+		return n-1
+	}
+
+	ans := f(slice, n-1, x)
+	return ans
+}
